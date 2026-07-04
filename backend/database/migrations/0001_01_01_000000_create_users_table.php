e8cbc981f9eaf3d8c4dafcbe7192c60ffa9d8bae
ArdiansyahYondri <ardiansyahyondriwardana@gmail.com>
2026-07-04 12:21:42 +0700

    Setup database, yaitu membuat migration untuk tabel users, kelas, materi, dan ujian

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('role', ['admin', 'guru', 'siswa'])->default('siswa'); // Tambahan kolom role
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
---
+++ backend/database/migrations/2026_07_04_051131_create_soals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('soal', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('ujian_id')->constrained('ujian')->cascadeOnDelete();
+        $table->text('pertanyaan');
+        $table->string('opsi_a');
+        $table->string('opsi_b');
+        $table->string('opsi_c');
+        $table->string('opsi_d');
+        $table->enum('kunci_jawaban', ['A', 'B', 'C', 'D']);
+        $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('soals');
+    }
+};
