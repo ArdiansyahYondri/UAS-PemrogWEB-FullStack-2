251d5f799ef24f401fc869faf47edcd632ea9150
ArdiansyahYondri <ardiansyahyondriwardana@gmail.com>
2026-07-05 19:17:26 +0700

    Memberikan dasaran pada code vue admin

---
+++ frontend/src/views/admin/KelolaPenggunaView.vue
@@ -0,0 +1,13 @@
+<script setup>
+// Logika javascript khusus admin di sini
+</script>
+
+<template>
+  <div>
+    <h1>Halaman Admin siap dikerjakan</h1>
+  </div>
+</template>
+
+<style scoped>
+/* Styling khusus admin */
+</style>
